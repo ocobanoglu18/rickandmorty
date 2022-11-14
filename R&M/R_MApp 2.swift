22c7f4db14d1bf6c4851f5b117daee09a52993bc
ocobanoglu18 <ocobanoglu18@ku.edu.tr>
2022-11-14 15:49:50 +0300

    Functions for cross fetching data added

---
+++ R&M/R_MApp 2.swift
@@ -0,0 +1,17 @@
+//
+//  R_MApp.swift
+//  R&M
+//
+//  Created by Adnan Cobanoglu on 2.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct R_MApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView().preferredColorScheme(.dark)
+        }
+    }
+}
